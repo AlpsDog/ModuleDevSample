f1afd33e1d9bb38ef8b6a0936d217720ec53a896
houshengli <1414006272@qq.com>
2019-08-19 17:47:39 +0800

    使用config.gradle统一版本，使用module.gradle复用

--- app/src/main/java/com/wonly/intellect/MainActivity.java
+++ app/src/main/java/com/wonly/intellect/MainActivity.java
@@ -1,8 +1,8 @@
 package com.wonly.intellect;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
